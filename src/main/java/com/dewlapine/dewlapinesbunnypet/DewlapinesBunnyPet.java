65d6b77af900ae9e721e2f76211e36ef6b3da0fd
Joey Dewlapine <joeytropigoo@proton.me>
2026-08-07 18:19:23 -0500

    Set up template, following video tutorial.

--- src/main/java/com/dewlapine/dewlapinesbunnypet/DewlapinesBunnyPet.java
+++ src/main/java/com/dewlapine/dewlapinesbunnypet/DewlapinesBunnyPet.java
@@ -24,7 +24,7 @@ public class DewlapinesBunnyPet {
 
     // The constructor for the mod class is the first code that is run when your mod is loaded.
     // FML will recognize some parameter types like IEventBus or ModContainer and pass them in automatically.
-    public DewlapinesBunnyPet (IEventBus modEventBus, ModContainer modContainer) {
+    public DewlapinesBunnyPet(IEventBus modEventBus, ModContainer modContainer) {
         // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
 
